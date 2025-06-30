c2a41b8d3672ffebf0232b0a5c30a8fb0554e0cc
stuut <daniel@stuut.com.au>
2025-06-30 10:31:49 +1000

    update supabase admin error

--- utils/supabase/superbaseAdmin.js
+++ utils/supabase/superbaseAdmin.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // Must NOT expose this to the client!
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY // Must NOT expose this to the client!
 )
